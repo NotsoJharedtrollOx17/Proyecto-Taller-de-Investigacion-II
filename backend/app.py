292db7913affd799400e1e4d95e2d93be083a0fb
NotsoJharedtroll23 <abraham.flores193@tectijuana.edu.mx>
2023-05-11 10:00:48 -0700

    REDACT: estructura de carpetas y rutas correctas

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,12 @@
 # lorem ipsum dolor...
+
+# TODO realizar API minima para conectar el frontend con el backend de flask
+from flask import Flask
+
+app = Flask(__name__)
+
+def main():
+    main()
+
+if __name__ == '__main__':
+    app.run()
