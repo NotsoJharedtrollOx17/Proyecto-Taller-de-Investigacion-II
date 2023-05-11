941888f172613ffac8feb7a646bcb8af23205cdf
NotsoJharedtroll23 <abraham.flores193@tectijuana.edu.mx>
2023-05-11 14:18:54 -0700

    REDACT: llamada de prueba al backend desde el frontend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 # lorem ipsum dolor...
 
 # TODO realizar API minima para conectar el frontend con el backend de flask
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+# Setting app route for proper startup headers  
 app = Flask(__name__)
+CORS(app)
 
-def main():
-    main()
+@app.route('/test/test_function', )
+def test():
+    return jsonify("TEXT FROM THE BACKEND :D")
+
 
 if __name__ == '__main__':
     app.run()
--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './css/App.css'
+import '../css/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [test, setTest] = useState('')
+  const [BACKEND_ROUTE] = useState(`http://localhost:5000/test/test_function`)
+
+  const fetchBackendRequest = () => {
+    fetch(BACKEND_ROUTE).
+    then((response) => response.json()).
+    then((data) => setTest(data)).
+    catch(error => {
+      console.error('Error fetching posts:', error);
+    })
+  }
 
   return (
     <>
@@ -21,10 +32,14 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
+      <div className='card'>
+      <button onClick={() => fetchBackendRequest()}>
+          llamar al backend!
+      </button>
+        <h1>{test}</h1>
+      </div>
+      +3.2601
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
